dd166831d99ddc81a795ab014a9378965bf1ee10
Kenishi <j.ardila1087@gmail.com>
2018-12-29 17:49:57 -0500

    Add Hamburguer menu
    Add video and image responsives

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+const burguerButton = document.querySelector('#burger-menu')
+
+const ipad = window.matchMedia('screen and (max-width: 767px)')
+ipad.addListener(validation)
+
+function validation(event) {
+    const { matches } = event
+    if (matches) {
+        burguerButton.addEventListener('click', toggleMemu)
+    }
+    else {
+        burguerButton.removeEventListener('click', toggleMemu)
+    }
+}
+
+function toggleMemu() {
+    const menu = document.querySelector(".menu")
+    menu.classList.toggle('is-active')
+}
+
+validation(ipad)
