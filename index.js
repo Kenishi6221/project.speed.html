d52351e6134682d9c4c6c75055d75d59296c2fbd
Kenishi <j.ardila1087@gmail.com>
2019-01-04 15:18:46 -0500

    Apply lazy load images

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { importImages } from './src/utils/import-images'
 require('./src/css/estilos.css')
 require('./src/js/menu')
+require('./src/js/lazyLoadImages')
 
 
 if (process.env.NODE_ENV !== 'production') {
